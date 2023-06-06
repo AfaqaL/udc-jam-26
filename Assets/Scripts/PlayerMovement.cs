d73674c9bac1ef79807e6035cfb770c9b17f9dc1
afaqa <lapak18@freeuni.edu.ge>
2023-06-07 02:13:24 +0400

    tilemapi da sprite, basic movementi

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private Camera mainCamera;
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        LookAtMouse();
+    }
+
+    private void LookAtMouse()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos = new Vector3(mousePos.x, mousePos.y, transform.position.z - mainCamera.transform.position.z);
+        mousePos = mainCamera.ScreenToWorldPoint(mousePos);
+        var direction = mousePos - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontalInput, verticalInput, 0f).normalized 
+                              * (moveSpeed * Time.deltaTime);;
+    }
+}
